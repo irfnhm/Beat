03e68a180a2d9dd82bf220e5a2ba90f442d39024
Irfan <irfnhm@gmail.com>
2018-07-20 19:42:48 +0530

    Final release

--- app/src/main/java/com/example/irfan/beat/PlayerActivity.java
+++ app/src/main/java/com/example/irfan/beat/PlayerActivity.java
@@ -2,11 +2,18 @@ package com.example.irfan.beat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class PlayerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+    }
+
+    public void changeState(View view){
+        Button activity =  (Button) findViewById(R.id.playState);
+        activity.setBackgroundResource(R.drawable.pause);
     }
 }
