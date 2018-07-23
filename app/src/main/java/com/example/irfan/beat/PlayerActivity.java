c34f2ae43b9ecb29c70028427e4f6ba80b6fc7f7
Irfan <irfnhm@gmail.com>
2018-07-23 21:57:30 +0530

    Changed onClick to OnClickListener

--- app/src/main/java/com/example/irfan/beat/PlayerActivity.java
+++ app/src/main/java/com/example/irfan/beat/PlayerActivity.java
@@ -1,8 +1,10 @@
 package com.example.irfan.beat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class PlayerActivity extends AppCompatActivity {
@@ -10,10 +12,15 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
-    }
 
-    public void changeState(View view){
-        Button activity =  (Button) findViewById(R.id.playState);
-        activity.setBackgroundResource(R.drawable.pause);
+        Button playSong = (Button) findViewById(R.id.playState);
+        playSong.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                Button activity = (Button) findViewById(R.id.playState);
+                activity.setBackgroundResource(R.drawable.pause);
+            }
+        });
     }
 }
